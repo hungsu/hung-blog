69a3c314cd2e590a369f0a4826d42611f8c0b52b
Hung-Su Nguyen <hungsu.nguyen@gmail.com>
2018-02-13 11:45:40 -0800

    Update site title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Hung-Su's blog" />
     <Navbar />
     <div>{children()}</div>
   </div>
